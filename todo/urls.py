85d5ffa24734d015a74db42cec383d1f19ded090
CholponSydykova <cholposha1995@gmail.com>
2021-01-25 13:47:57 +0600

    added todo deletion

--- todo/urls.py
+++ todo/urls.py
@@ -29,7 +29,8 @@ urlpatterns = [
     path("change/", change),
     path("delete/", delete),
     path("books/", books , name ="books"),
-    path("add-todo/", add_todo, name="add-todo")
+    path("add-todo/", add_todo, name="add-todo"),
+    path("delete-todo/<id>/", delete_todo, name="delete-todo"),
 
-    ]   +static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)\
+]   +static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)\
     +static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
