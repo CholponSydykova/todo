3f8725de28a51a8e10b9a7fe53529abb62bd5e8a
CholponSydykova <cholposha1995@gmail.com>
2021-01-25 22:43:21 +0600

    set delete button, markwd and unmarked icons

--- todo/urls.py
+++ todo/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from main.views import *
+from main.views import homepage, test, second, third, add, change, delete, books, delete_todo, add_todo, mark_todo
+
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -31,6 +32,7 @@ urlpatterns = [
     path("books/", books , name ="books"),
     path("add-todo/", add_todo, name="add-todo"),
     path("delete-todo/<id>/", delete_todo, name="delete-todo"),
+    path("mark-todo/<id>/", mark_todo, name="mark-todo"),
 
 ]   +static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)\
     +static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
